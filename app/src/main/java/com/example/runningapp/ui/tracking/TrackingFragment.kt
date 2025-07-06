cd8d57f726e701b7e6ae583d56a863af4ff84665
f-cet <f-cetiner99@hotmail.com>
2025-07-06 17:57:54 +0300

    Tracking service basic setup completed

--- app/src/main/java/com/example/runningapp/ui/tracking/TrackingFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/tracking/TrackingFragment.kt
@@ -1,22 +1,31 @@
 package com.example.runningapp.ui.tracking
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningapp.R
 import com.example.runningapp.databinding.FragmentRunBinding
 import com.example.runningapp.databinding.FragmentTrackingBinding
+import com.example.runningapp.services.TrackingService
 import com.example.runningapp.ui.MainViewModel
+import com.example.runningapp.util.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.getValue
 
 
 @AndroidEntryPoint
-class TrackingFragment: Fragment() {
+class TrackingFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -33,13 +42,34 @@ class TrackingFragment: Fragment() {
         return binding.root
     }
 
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.mapView.onCreate(savedInstanceState)
+
+        binding.btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         binding.mapView.getMapAsync {
             map = it
+            map?.isMyLocationEnabled = true
+
+            val fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(requireContext())
+
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val currentLatLng = LatLng(it.latitude, it.longitude)
+                    map?.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                }
+            }
         }
+    }
+
+    private fun sendCommandToService(action: String) = Intent(requireContext(), TrackingService::class.java).also {
+        it.action = action
+        requireContext().startService(it)
     }
 
     override fun onResume() {
@@ -77,3 +107,4 @@ class TrackingFragment: Fragment() {
         _binding = null
     }
 }
+

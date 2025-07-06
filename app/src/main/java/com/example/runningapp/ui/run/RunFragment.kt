9720b0ab9bb62023a0c711f0aed26bd98623d14c
f-cet <f-cetiner99@hotmail.com>
2025-07-06 17:00:58 +0300

    Handling locations permissions

--- app/src/main/java/com/example/runningapp/ui/run/RunFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/run/RunFragment.kt
@@ -1,10 +1,13 @@
-@file:Suppress("TYPE_INTERSECTION_AS_REIFIED_WARNING",
+@file:Suppress(
+    "TYPE_INTERSECTION_AS_REIFIED_WARNING",
     "INFERRED_TYPE_VARIABLE_INTO_EMPTY_INTERSECTION_WARNING"
 )
 
 package com.example.runningapp.ui.run
 
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,13 +17,16 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.runningapp.R
 import com.example.runningapp.databinding.FragmentRunBinding
-import com.example.runningapp.databinding.FragmentSetupBinding
 import com.example.runningapp.ui.MainViewModel
+import com.example.runningapp.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.runningapp.util.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @Suppress("TYPE_INTERSECTION_AS_REIFIED_WARNING")
 @AndroidEntryPoint
-class RunFragment: Fragment() {
+class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -38,13 +44,68 @@ class RunFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermissions()
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permission to use this app",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permission to use this app",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(
+        requestCode: Int,
+        perms: MutableList<String?>
+    ) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(
+        requestCode: Int,
+        perms: MutableList<String?>
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }

26b2ba3c0dd3dd372eac49873cde18bb7b2855e6
f-cet <f-cetiner99@hotmail.com>
2025-07-11 00:01:32 +0300

    Tracking User Location in the Background completed

--- app/src/main/java/com/example/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/example/runningapp/services/TrackingService.kt
@@ -1,6 +1,8 @@
 package com.example.runningapp.services
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
@@ -9,32 +11,76 @@ import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.room.InvalidationTracker
 import com.example.runningapp.R
 import com.example.runningapp.ui.MainActivity
 import com.example.runningapp.util.Constants.ACTION_PAUSE_SERVICE
 import com.example.runningapp.util.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.example.runningapp.util.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.runningapp.util.Constants.ACTION_STOP_SERVICE
+import com.example.runningapp.util.Constants.FASTEST_LOCATION_INTERVAL
+import com.example.runningapp.util.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.runningapp.util.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.runningapp.util.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.runningapp.util.Constants.NOTIFICATION_ID
 import com.example.runningapp.util.Constants.RUNNING_APP
+import com.example.runningapp.util.TrackingUtility
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues(){
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer{
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
                     if (isFirstRun) {
-                        startForegroundService()
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            startForegroundService()
+                        }
                         isFirstRun = false
                     } else {
                         Timber.d("Resuming service...")
@@ -54,7 +100,61 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun addPathPoint(location: Location?){
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun updateLocationTracking(isTracking: Boolean){
+        if (isTracking){
+            if (TrackingUtility.hasLocationPermissions(this)){
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        }else{
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if (isTracking.value!!){
+                result?.locations?.let { locations ->
+                    for (location in locations){
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     private fun startForegroundService() {
+
+        addEmptyPolyline()
+        isTracking.postValue(true)
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
                 as NotificationManager
 
@@ -71,7 +171,11 @@ class TrackingService : LifecycleService() {
             .setContentIntent(getMainActivityPendingIntent())
 
         //startForeground(NOTIFICATION_ID, notificationBuilder.build())
-        startForeground(NOTIFICATION_ID, notificationBuilder.build(), ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION)
+        startForeground(
+            NOTIFICATION_ID,
+            notificationBuilder.build(),
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION
+        )
 
     }
 
@@ -95,3 +199,4 @@ class TrackingService : LifecycleService() {
     }
 
 }
+
